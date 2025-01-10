9d8a2a65f4e36d6d1b83b5f4650bb7f30b93f0ae
layfmeg <116550480+layfmeg@users.noreply.github.com>
2025-01-10 16:31:58 +0200

    Update Main.java

--- Arrays/src/main/java/Main.java
+++ Arrays/src/main/java/Main.java
@@ -3,15 +3,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] largeArray = new String[20];
-        String[] firstArray = new String[10];
-        String[] secondArray = new String[10];
+        int[] largeArray = new int[20];
+        int[] firstHalf = new int[10];
+        int[] secondHalf = new int[10];
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите 20 строк:");
         for (int i = 0; i < largeArray.length; i++) {
-            largeArray[i] = scanner.nextLine();
+            largeArray[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            firstHalf[i] = largeArray[i];
+            secondHalf[i] = largeArray[i + 10];
+        }
 
+        System.out.println("Первая половина массива: ");
+        for (int i = 0; i < firstHalf.length; i++) {
+            System.out.println(firstHalf[i]);
+        }
+
+        System.out.println("Вторая половина массива: ");
+        for (int i = 0; i < secondHalf.length; i++) {
+            System.out.println(secondHalf[i]);
         }
     }
-}
+    }
